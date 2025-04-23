8066037dc4e1deeb37478c51b5a4822db1a7c490
savchan <schan55@jh.edu>
2025-04-23 19:44:18 -0400

    changes to main_3

--- classification/main_3.py
+++ classification/main_3.py
@@ -39,17 +39,17 @@ def main_3():
     train_metrics = train(model, train_loader, val_loader, pos_ratio, device, epochs=args.num_epochs, lr=args.learning_rate)
 
     #TODO: add appropriate results_path when running the code
-    plot_loss(train_metrics, args.results_path, "model_2")
+    plot_loss(train_metrics, args.results_path, "model_3")
 
     # Save the model 
     #TODO: change the model path to the right name
-    best_loss_ckpt = os.path.join(args.results_path, 'model2_best_loss.pth')
+    best_loss_ckpt = os.path.join(args.results_path, 'model3_best_loss.pth')
     state = torch.load(best_loss_ckpt, map_location=device)
     model.load_state_dict(state['model_state_dict'])
     print("Best-loss: ", evaluate(model, test_loader, device))
 
     # evaluate checkpoint with best validation accuracy
-    best_acc_ckpt = os.path.join(args.results_path, 'model2_best_acc.pth')
+    best_acc_ckpt = os.path.join(args.results_path, 'model3_best_acc.pth')
     state = torch.load(best_acc_ckpt, map_location=device)
     model.load_state_dict(state['model_state_dict'])
     print("Best-acc: ", evaluate(model, test_loader, device))
