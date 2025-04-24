4e1d9ff57f497713a67abea14de8ec4d691f6790
isalisss <isaliskleperd@gmail.com>
2025-04-24 09:49:03 -0400

    modif train val test split ddi

--- classification/main_5.py
+++ classification/main_5.py
@@ -27,7 +27,7 @@ def main():
     df_DDI = load_ddi_metadata('../data/ddi_cropped/ddi_metadata.csv')
     df_AUG = load_generated_metadata('../data/HAM10000/HAM10000_metadata.csv', '../data/darkHAM_sigproc')   # TODO: change the path to the right name 
     train_ham_df, val_ham_df, test_ham_df = split_data(df_HAM)
-    train_ddi_df, val_ddi_df, test_ddi_df = split_data(df_DDI)
+    test_ddi_df, val_ddi_df, train_ddi_df = split_data(df_DDI)
     df_AUG['image_id'] = df_AUG['image_path'].apply(lambda x: x.split('_fake_Y')[0]) + '.jpg'
     train_aug_df = df_AUG[(df_AUG['image_id']).isin(train_ham_df['image_path'])][['id','image_path','label']]
     pos_ratio = get_pos_ratio(pd.concat([train_ham_df, train_ddi_df, train_aug_df]))
