3fa84fdbba50a1e6f64dfb5278b30add0b656a52
rlagksk0315 <hbananakim@gmail.com>
2025-05-12 12:53:44 -0400

    model5 results

--- classification/train_model1.sh
+++ classification/train_model1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CUDA_VISIBLE_DEVICES=1
-export results_path='../results/hana/main_1_classification'
+export results_path='../results/hana/final_main_1_classification'
 
 python main_1.py --results_path "$results_path" \
                                --num_epochs 200 \
--- classification/train_model5.sh
+++ classification/train_model5.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=2
-export results_path='../results/hana/main_5_classification'
+export results_path='../results/hana/final_main_5_classification'
 
 python main_5.py --results_path "$results_path" \
-                 --num_epochs 100 \
+                 --num_epochs 200 \
                  --learning_rate 0.001
